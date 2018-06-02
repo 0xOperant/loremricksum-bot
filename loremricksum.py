e47bff8d007b39a4d036ed14a4ea77f60706e1c7
belldavidr <belldavidr@gmail.com>
2018-06-02 19:19:08 -0400

    moar streaming

--- loremricksum.py
+++ loremricksum.py
@@ -1,12 +1,11 @@
 #!/usr/bin/env python
 import os, json, io, time, urllib.request, urllib.error, urllib.parse
 import tweepy
-from creds import *
 
-CONSUMER_KEY = ['CONSUMER_KEY']
-CONSUMER_SECRET = ['CONSUMER_SECRET']
-ACCESS_TOKEN = ['ACCESS_TOKEN']
-ACCESS_SECRET = ['ACCESS_SECRET']
+CONSUMER_KEY = os.environ['CONSUMER_KEY']
+CONSUMER_SECRET = os.environ['CONSUMER_SECRET']
+ACCESS_TOKEN = os.environ['ACCESS_TOKEN']
+ACCESS_SECRET = os.environ['ACCESS_SECRET']
 
 auth = tweepy.OAuthHandler(CONSUMER_KEY, CONSUMER_SECRET)
 auth.set_access_token(ACCESS_TOKEN, ACCESS_SECRET)
